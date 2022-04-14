6baf2580ecac3e8e3ec61985cb75b9f72681a582
Murat Alkan <muratalkanw@hotmail.com>
2022-04-14 17:32:19 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	Pkg "iotwin/main/pkg"
+)
 
 func main() {
-	fmt.Println("iotwin")
+	fmt.Println("***Main***")
+
+	// Init
+	Pkg.InitLogger()
+	Pkg.InitHelper()
+
+	// MQTT
+	Pkg.CreateMQTTClient()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	signal.Notify(sig, syscall.SIGTERM)
+	<-sig
 }
