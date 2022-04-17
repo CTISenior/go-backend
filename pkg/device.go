2516fad575daf75570cccf04af6be8dd16f17143
Murat Alkan <muratalkanw@hotmail.com>
2022-04-17 21:00:48 +0000

    Add new device structure

--- pkg/device.go
+++ pkg/device.go
@@ -1,9 +1,21 @@
 package pkg
 
+import (
+	"github.com/google/uuid"
+)
+
 type Device struct {
 	sn     string
 	ts     int
 	values map[string]string
+}
+
+type DeviceDB struct {
+	ID        *uuid.UUID `bun:",pk,type:uuid,default:uuid_generate_v4()"`
+	AssetID   *uuid.UUID `bun:",pk,type:uuid,default:uuid_generate_v4()"`
+	TenantID  *string
+	Types     []string
+	MaxValues []string
 }
 
 type Telemetry struct {
